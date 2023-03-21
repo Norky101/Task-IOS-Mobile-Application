b54212eb5ca6b975ad29bd5cd14f699dfd17573f
Noah Pilkington <82910305+Norky101@users.noreply.github.com>
2023-03-20 23:54:56 -0400

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskSquirrel
+//
+//  Created by Noah Pilkington on 3/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
